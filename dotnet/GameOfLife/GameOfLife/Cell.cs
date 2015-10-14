fd4e82ecd662def74dc8ce9bf819d3c2320c696e
Kate <katerebekah@gmail.com>
2015-10-14 17:01:40 -0500

    beginnings

---
+++ dotnet/GameOfLife/GameOfLife/Cell.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    class Cell
+    {
+        public Cell (int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public bool IsAlive { get; set; }
+
+        public int neightbors { get; set; }
+
+        public int x { get; set; }
+
+        public int y { get; set; }
+
+    }
+}
---
+++ dotnet/GameOfLife/GoLTest/CellTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GoLTest
+{
+    [TestClass]
+    public class CellTest
+    {
+        //Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+
+        [TestMethod]
+        public void CellDiesWithUnderPopulation()
+        {
+            
+        }
+
+        //Any live cell with two or three live neighbours lives on to the next generation.
+
+        [TestMethod]
+        public void CellWith2or3NeighborsLives()
+        {
+
+        }
+
+        //Any live cell with more than three live neighbours dies, as if by overcrowding.
+
+        [TestMethod]
+        public void CellDiesWithOverPopulation()
+        {
+
+        }
+
+        //Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+        [TestMethod]
+        public void CellReproducesWith3Neighbors()
+        {
+
+        }
+    }
+}
